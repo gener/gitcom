231bd66dfb7086ec2d91e710ffdefda79e8b4074
Eugene Kalyada <mail@color-rain.ru>
2019-03-09 23:59:16 +0500

    Initial Commit

---
+++ gitcmt/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gitcmt
+//
+//  Created by Eugene Kalyada on 09/03/2019.
+//  Copyright © 2019 EDCODE. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
